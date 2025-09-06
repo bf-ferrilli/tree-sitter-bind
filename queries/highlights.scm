580bdf44f4bebeaf94f3da8958eacd26fcc48a7b
Brandon Froehlich <froehlich@fpc-24-48737102-l.tail8b348c.ts.net>
2025-09-05 23:08:25 -0400

    Dramatically improve syntax highlighting differentiation
    Major improvements to visual distinction:
    
    - TTL values: Now use @markup.italic (directive) vs @markup.italic (record)
    - MX priorities: @markup.underline.priority for high visibility
    - SRV numbers: Different colors for priority/weight/port
    - Record names: @entity.name.function (left column names)
    - Target domains: @string.regexp (right side destinations)
    - IP addresses: @string.escape for maximum distinction
    - SOA serials: @constant.numeric.hex to stand out from times
    - TXT content: @string.quoted.double for quoted strings
    - @ symbol: @keyword.operator for special treatment
    
    Should provide much better visual separation between all element types.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -4,74 +4,75 @@
 ; Directives
 ["" "" ""] @keyword.directive
 
-; Resource record fields with proper context-based highlighting
+; Resource record fields with MUCH more distinct highlighting
 (resource_record
-  name: (domain_name) @entity.name.tag
-  ttl: (time_value) @constant.numeric.time
+  name: (domain_name) @entity.name.function
+  ttl: (time_value) @markup.italic
   class: (record_class) @constant.builtin
   type: (record_type) @type.builtin
   data: (_) @string.special)
 
-; Directive time values (different from record TTL)  
+; Directive time values - make them stand out more
 (ttl_directive
-  (time_value) @constant.numeric.time)
+  (time_value) @markup.bold)
 
-; Record types and classes (when not in field context)
+; Record types and classes
 (record_type) @type.builtin
 (record_class) @constant.builtin
 
-; Domain names in different contexts
-; Record names (left side) - entity names
-(resource_record name: (domain_name) @entity.name.tag)
-; Domain names in data (right side) - strings
-(domain_name) @variable.other
+; Domain names with MUCH better differentiation
+; Record names (left column) - make them standout entities
+(resource_record name: (domain_name) @entity.name.function)
 
-; Special domain references
-(domain_name "@") @constant.builtin.special
+; Special @ symbol
+(domain_name "@") @keyword.operator
 
-; Special symbols and punctuation
+; Domain names in record data - target destinations
+(mx_data (domain_name) @string.regexp)
+(srv_data (domain_name) @string.regexp)
+(soa_data (domain_name) @string.regexp (domain_name) @string.regexp)
+(generic_data (domain_name) @string.regexp)
+
+; All other domain names fallback
+(domain_name) @variable.other.member
+
+; Punctuation
 ["@" "(" ")"] @punctuation.special
 
-; Numbers in different contexts - using different highlight categories
+; Numbers with AGGRESSIVE differentiation by context
+; MX priorities - make them pop
 (mx_data
-  (number) @variable.parameter.priority
-  (domain_name) @string.other.target)
+  (number) @markup.underline.priority)
 
+; SRV numbers - each gets different treatment  
 (srv_data
-  (number) @variable.parameter.priority
-  (number) @variable.parameter.weight  
-  (number) @variable.parameter.port
-  (domain_name) @string.other.target)
+  (number) @markup.underline.priority
+  (number) @variable.parameter.weight
+  (number) @constant.character.port)
 
+; SOA numbers - serial vs times
 (soa_data
-  (domain_name) @string.other.server
-  (domain_name) @string.other.email
-  (number) @constant.numeric.serial
-  (time_value) @constant.numeric.time
-  (time_value) @constant.numeric.time
-  (time_value) @constant.numeric.time
-  (time_value) @constant.numeric.time)
+  (number) @constant.numeric.hex
+  (time_value) @markup.italic
+  (time_value) @markup.italic
+  (time_value) @markup.italic
+  (time_value) @markup.italic)
 
-; IP addresses get special treatment - more distinct
-(ipv4_address) @constant.numeric.address
-(ipv6_address) @constant.numeric.address
+; IP addresses - make them REALLY stand out
+(ipv4_address) @string.escape
+(ipv6_address) @string.escape
 
-; Generic numbers and time values (fallback)
+; TTL values in records - different from directive TTL
+(resource_record ttl: (time_value) @markup.italic)
+
+; Generic fallbacks
 (number) @number
-(time_value) @constant.numeric.time
+(time_value) @markup.italic
 
-; Strings and paths
+; Strings
 (quoted_string) @string
 (file_path) @string.special.path
 
-; TXT record data
+; TXT record content - make quoted strings more obvious
 (txt_data
-  (quoted_string) @string)
-
-; Generic data fallback with more specific targeting
-(generic_data
-  (domain_name) @string.other.target
-  (ipv4_address) @constant.numeric.address
-  (ipv6_address) @constant.numeric.address
-  (number) @number
-  (quoted_string) @string)
+  (quoted_string) @string.quoted.double)
